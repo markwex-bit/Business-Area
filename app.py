c5fff770624391cd32c46e8b0518607c166e7e2b
markwex-bit <markwex@gmail.com>
2025-06-21 17:44:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-import streamlit as st
 import os
 from openai import OpenAI
-from dotenv import load_dotenv
 
-# Load OpenAI API key
-load_dotenv()
 openai_api_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=openai_api_key)
+
 
 # Core LLM chat function
 def chat(messages):
